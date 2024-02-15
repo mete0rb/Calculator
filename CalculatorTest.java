c833407f50c89bf2e0a5ea24fbd483ebade3d820
mete0rb <153194582+mete0rb@users.noreply.github.com>
2024-02-15 23:54:47 +0330

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5, Calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(3, Calculator.subtract(6, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(12, Calculator.multiply(4, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.5, Calculator.divide(5, 2), 0.0001);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        Calculator.divide(5, 0);
+    }
+}
